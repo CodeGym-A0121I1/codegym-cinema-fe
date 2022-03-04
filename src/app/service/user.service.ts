71d78fd6fde487ed8eabe99cddc6dce48724c211
Your Name <you@example.com>
2022-03-05 02:09:31 +0700

    Create Module (Component + Service + Routing)

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,25 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {ForgotPassword} from "../dto/user/ForgotPassword";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  API_USER = "http://localhost:8080/api/users"
+    API_USER = "http://localhost:8080/api/users"
 
-  requestHeader = new HttpHeaders(
-      {"No-Auth": "True"}
-  );
+    requestHeader = new HttpHeaders(
+        {"No-Auth": "True"}
+    );
 
-  constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient) {
+    }
 
-  public generateOtp(username: string): Observable<Boolean> {
-    return this.httpClient.get<Boolean>(this.API_USER + "/account/generate/" + username, {headers: this.requestHeader});
-  }
+    public generateOtp(username: string): Observable<Boolean> {
+        return this.httpClient.get<Boolean>(this.API_USER + "/account/generate/" + username, {headers: this.requestHeader});
+    }
 
-  public forgotPassword(username: string, newPassword: string, otp: string): Observable<Boolean> {
-    return this.httpClient.get<Boolean>(this.API_USER + "/account/forgot-password/" + username + "/" + newPassword + "/" + otp, {headers: this.requestHeader});
-  }
+    public forgotPassword(forgotPassword: ForgotPassword): Observable<Boolean> {
+        return this.httpClient.post<Boolean>(this.API_USER + "/account/forgot-password", forgotPassword, {headers: this.requestHeader});
+    }
 }
