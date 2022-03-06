52fa51f2519835b03cb62e25edafec64288d85b2
nguyenthinga-0206 <nga3421@gmail.com>
2022-03-06 18:33:03 +0700

    sfgg

--- src/app/component/booking/list-booking/list-booking.component.ts
+++ src/app/component/booking/list-booking/list-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BookingService} from "../../../service/booking.service";
 
 @Component({
   selector: 'app-list-booking',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListBookingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bookingService: BookingService) { }
 
   ngOnInit(): void {
+
   }
 
 }
