748750fd2de811725dd4e66b5ef59573db5dc247
Le Hoang Long <lelong.ll32@gmail.com>
2022-03-09 15:33:21 +0700

    Update Statistic

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {StatisticMovieDTO} from "../dto/statistic/StatisticMovieDTO";
 
@@ -7,13 +7,14 @@ import {StatisticMovieDTO} from "../dto/statistic/StatisticMovieDTO";
   providedIn: 'root'
 })
 export class MovieService {
-
+  headers = new HttpHeaders(({
+    'Authorization': "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWhvYW5nbG9uZyIsImV4cCI6MTY0NjgxNzQ4MiwiaWF0IjoxNjQ2Mzg1NDgyfQ.wgNJAi4ILFKoacgIlaoDvifZOx6oYZVgeSqa_wBtwXk"
+  }))
   private readonly STATISTIC_MOVIE_API: string = "http://localhost:8080/api/statistic/movie";
 
   constructor(private http: HttpClient) { }
 
   public statisticTopMovieByGrossing(): Observable<StatisticMovieDTO[]> {
-    console.log(this.http.get<StatisticMovieDTO[]>(`${this.STATISTIC_MOVIE_API}`));
-    return this.http.get<StatisticMovieDTO[]>(`${this.STATISTIC_MOVIE_API}`);
+    return this.http.get<StatisticMovieDTO[]>(`${this.STATISTIC_MOVIE_API}`, {headers: this.headers});
   }
 }
