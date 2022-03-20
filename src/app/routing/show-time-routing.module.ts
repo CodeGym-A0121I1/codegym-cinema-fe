623824a243bb0885dcdc69da37454cece4fbb0fe
Uyen Nguyen <nguyenuyen552000@gmail.com>
2022-03-20 23:25:53 +0700

    fix routing

--- src/app/routing/show-time-routing.module.ts
+++ src/app/routing/show-time-routing.module.ts
@@ -4,10 +4,13 @@ import {
     SelectedMovieShowTimeComponent
 } from "../component/show-time/selected-movie-show-time/selected-movie-show-time.component";
 import {SelectedSeatComponent} from "../component/show-time/selected-seat/selected-seat.component";
+import {AuthGuard} from "../guard/auth.guard";
 
 const routes: Routes = [
-    { path: "selectedShowTime/:id", component: SelectedMovieShowTimeComponent  },
-    { path: "selectedSeat", component: SelectedSeatComponent  }
+    { path: "selectedShowTime/:id", component: SelectedMovieShowTimeComponent,canActivate: [AuthGuard],
+        data: {role: ['ROLE_USER']}},
+    { path: "selectedSeat", component: SelectedSeatComponent,canActivate: [AuthGuard],
+        data: {role: ['ROLE_USER']}}
 ];
 
 @NgModule({
